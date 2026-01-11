192184bf8f1eab2fac08321b6a40bb7419c67c82
kahol <kahola@gmail.com>
2026-01-11 17:19:35 +0100

    Add project files.

---
+++ calc/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace calc;
+
+public class Calculator
+{
+	public int Addition(int x, int y)
+	{
+		return x + y;
+	}
+
+	public int Subtraction(int x, int y)
+	{
+		return x - y;
+	}
+
+	public int Multiplication(int x, int y)
+	{
+		return x * y;
+	}
+
+	public double Divide(int x, int y)
+	{
+		return x / y;
+	}
+}
---
+++ pipelinbe-demo/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using calc;
+
+namespace pipelinbe_demo;
+
+public class Tests
+{
+	private Calculator calculator;
+	[SetUp]
+	public void Setup()
+	{
+		calculator = new Calculator();
+	}
+
+	[Test]
+	public void Addition()
+	{
+		// Arrange
+		int first = 1;
+		int second = 2;
+		// Act
+
+		var result = calculator.Addition(first, second);
+		// Assert
+
+		Assert.That(result, Is.EqualTo(first + second));
+	}
+}
