e32db64e71c4255ba5e8d32988caac9637a6c8bc
kahol <kahola@gmail.com>
2026-01-11 17:25:53 +0100

    added tests

--- pipelinbe-demo/UnitTest1.cs
+++ pipelinbe-demo/UnitTest1.cs
@@ -24,4 +24,47 @@ public class Tests
 
 		Assert.That(result, Is.EqualTo(first + second));
 	}
+
+	[Test]
+	public void Subtraction()
+	{
+		// Arrange
+		int first = 1;
+		int second = 2;
+		// Act
+
+		var result = calculator.Subtraction(first, second);
+		// Assert
+
+		Assert.That(result, Is.EqualTo(first - second));
+	}
+
+	[Test]
+	public void Multiplication()
+	{
+		// Arrange
+		int first = 1;
+		int second = 2;
+		// Act
+
+		var result = calculator.Multiplication(first, second);
+		// Assert
+
+		Assert.That(result, Is.EqualTo(first * second));
+	}
+
+	[Test]
+	public void Division()
+	{
+		// Arrange
+		int first = 1;
+		int second = 2;
+		// Act
+
+		var result = calculator.Divide(first, second);
+		// Assert
+
+		Assert.That(result, Is.EqualTo(first / second));
+	}
+
 }
