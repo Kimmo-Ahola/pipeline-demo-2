5ee6ea4245ce880ca1462dfe1a283135b9ba9a4f
kahol <kahola@gmail.com>
2026-01-11 17:34:59 +0100

    adde UI test for chrome

---
+++ pipelinbe-demo/SeleniumChromeTests.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace pipelinbe_demo;
+
+[Category("UI")]
+public class SeleniumChromeTests
+{
+	private ChromeDriver _chromeDriver;
+	private const string BaseURL = "https://www.kimmoahola.net/selenium.html";
+
+	[SetUp]
+	public void Setup()
+	{
+		new DriverManager().SetUpDriver(new ChromeConfig());
+
+		var options = new ChromeOptions();
+		options.AddArgument("--headless");
+		_chromeDriver = new ChromeDriver(options);
+		_chromeDriver.Navigate().GoToUrl(BaseURL);
+	}
+
+	[TearDown]
+	public void Teardown()
+	{
+		_chromeDriver.Dispose();
+	}
+
+	[Test]
+	public void GetMainTitleText()
+	{
+		var title = _chromeDriver.FindElement(By.Id("main-title")).Text;
+		Assert.That(title, Is.EqualTo("Selenium Test Page"));
+	}
+}
