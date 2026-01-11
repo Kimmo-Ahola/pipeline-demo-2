6658839c9319db5a29c86160614b9b10d072f23a
kahol <kahola@gmail.com>
2026-01-11 17:37:42 +0100

    Added more ui tests

--- pipelinbe-demo/SeleniumFirefoxTests.cs
+++ pipelinbe-demo/SeleniumFirefoxTests.cs
@@ -1,6 +1,6 @@
 ﻿using NUnit.Framework.Legacy;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
@@ -8,39 +8,41 @@ using WebDriverManager.DriverConfigs.Impl;
 namespace pipelinbe_demo;
 
 [Category("UI")]
-public class SeleniumChromeTests
+public class SeleniumFirefoxTests
 {
-	private ChromeDriver _chromeDriver;
+	private FirefoxDriver _firefoxDriver;
 	private const string BaseURL = "https://www.kimmoahola.net/selenium.html";
 
 	[SetUp]
 	public void Setup()
 	{
-		new DriverManager().SetUpDriver(new ChromeConfig());
+		new DriverManager().SetUpDriver(new FirefoxConfig());
 
-		var options = new ChromeOptions();
+		var options = new FirefoxOptions();
 		options.AddArgument("--headless");
-		_chromeDriver = new ChromeDriver(options);
-		_chromeDriver.Navigate().GoToUrl(BaseURL);
+
+		_firefoxDriver = new FirefoxDriver(options);
+		_firefoxDriver.Navigate().GoToUrl(BaseURL);
 	}
 
 	[TearDown]
 	public void Teardown()
 	{
-		_chromeDriver.Dispose();
+		_firefoxDriver.Dispose();
+		_firefoxDriver.Quit();
 	}
 
 	[Test]
 	public void GetMainTitleText()
 	{
-		var title = _chromeDriver.FindElement(By.Id("main-title")).Text;
+		var title = _firefoxDriver.FindElement(By.Id("main-title")).Text;
 		Assert.That(title, Is.EqualTo("Selenium Test Page"));
 	}
 
 	[Test]
 	public void EnterUsername()
 	{
-		var usernameInput = _chromeDriver.FindElement(By.Name("username"));
+		var usernameInput = _firefoxDriver.FindElement(By.Name("username"));
 		usernameInput.Clear();
 		usernameInput.SendKeys("testuser");
 		Assert.That(usernameInput.GetAttribute("value"), Is.EqualTo("testuser"));
@@ -49,7 +51,7 @@ public class SeleniumChromeTests
 	[Test]
 	public void CheckCheckbox2()
 	{
-		var checkbox = _chromeDriver.FindElement(By.Name("check2"));
+		var checkbox = _firefoxDriver.FindElement(By.Name("check2"));
 		if (!checkbox.Selected)
 		{
 			checkbox.Click();
@@ -60,7 +62,7 @@ public class SeleniumChromeTests
 	[Test]
 	public void SelectBlueRadio()
 	{
-		var radio = _chromeDriver.FindElement(By.CssSelector("input[type='radio'][value='blue']"));
+		var radio = _firefoxDriver.FindElement(By.CssSelector("input[type='radio'][value='blue']"));
 		radio.Click();
 		Assert.That(radio.Selected);
 	}
@@ -68,7 +70,7 @@ public class SeleniumChromeTests
 	[Test]
 	public void SelectDropdownOption3()
 	{
-		var dropdown = _chromeDriver.FindElement(By.Id("dropdown"));
+		var dropdown = _firefoxDriver.FindElement(By.Id("dropdown"));
 		var dropdownElement = new SelectElement(dropdown);
 		dropdownElement.SelectByText("Option 3");
 		Assert.That(dropdownElement.SelectedOption.GetAttribute("value"), Is.EqualTo("option3"));
@@ -77,7 +79,7 @@ public class SeleniumChromeTests
 	[Test]
 	public void ClickButtonByClass()
 	{
-		var button = _chromeDriver.FindElement(By.ClassName("btn-class"));
+		var button = _firefoxDriver.FindElement(By.ClassName("btn-class"));
 		button.Click();
 		Assert.Pass("Button clicked successfully");
 	}
@@ -85,7 +87,7 @@ public class SeleniumChromeTests
 	[Test]
 	public void GetLinkHrefWithoutNavigating()
 	{
-		var link = _chromeDriver.FindElement(By.Id("link-id"));
+		var link = _firefoxDriver.FindElement(By.Id("link-id"));
 		var href = link.GetAttribute("href");
 		ClassicAssert.IsNotNull(href);
 	}
@@ -93,7 +95,7 @@ public class SeleniumChromeTests
 	[Test]
 	public void GetNestedSpanText()
 	{
-		var span = _chromeDriver.FindElement(By.CssSelector(".outer .inner #nested-span"));
+		var span = _firefoxDriver.FindElement(By.CssSelector(".outer .inner #nested-span"));
 		var text = span.Text;
 		Assert.That(text, Is.EqualTo("Nested Span"));
 	}
@@ -101,7 +103,7 @@ public class SeleniumChromeTests
 	[Test]
 	public void GetImageAltText()
 	{
-		var image = _chromeDriver.FindElement(By.Id("image-id"));
+		var image = _firefoxDriver.FindElement(By.Id("image-id"));
 		var alt = image.GetAttribute("alt");
 		Assert.That(alt, Is.EqualTo("Did you find the alt text?"));
 	}
